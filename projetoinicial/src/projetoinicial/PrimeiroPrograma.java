a4e0a29aa9204cf3fd823cb008f387cad3fa99f6
Adriano <adrianops31@gmail.com>
2021-02-17 22:53:24 -0200

    eclipse

--- projetoinicial/src/projetoinicial/PrimeiroPrograma.java
+++ projetoinicial/src/projetoinicial/PrimeiroPrograma.java
@@ -1,7 +1,13 @@
 package projetoinicial;
 
+import java.util.Date;
+
 public class PrimeiroPrograma {
 public static void main(String[] args) {
 	System.out.println("CARALHO");
+	
+	Date data = new Date();
+	System.out.println("A hora do sistema é");
+	System.out.println(data.toString());
 }
 }
