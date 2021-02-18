4ac631cff8cfbb6ffc719add6b13c1e11980fa80
Adriano <adrianops31@gmail.com>
2021-02-17 23:16:47 -0200

    tst functions

--- projetoinicial/src/projetoinicial/PrimeiroPrograma.java
+++ projetoinicial/src/projetoinicial/PrimeiroPrograma.java
@@ -1,13 +1,27 @@
 package projetoinicial;
 
-import java.util.Date;
+//import java.awt.Dimension;
+//import java.awt.Toolkit;
+//import java.util.Date;
+//import java.util.Locale;
 
 public class PrimeiroPrograma {
 public static void main(String[] args) {
 	System.out.println("CARALHO");
 	
-	Date data = new Date();
-	System.out.println("A hora do sistema é");
-	System.out.println(data.toString());
+//	Date data = new Date();
+//	System.out.println("A hora do sistema é");
+//	System.out.println(data.toString());
+	
+	
+//	Locale idioma = Locale.getDefault();
+//	System.out.println("O idioma do sistema é");
+//	System.out.println(idioma.toString());
+//	System.out.println(idioma.getDisplayLanguage());
+	
+//    Dimension tela = Toolkit.getDefaultToolkit().getScreenSize();
+//    int lar = (int) tela.getWidth();
+//    int alt = (int) tela.getHeight();
+//    System.out.println("Sua tela tem resolução " + lar + " x " + alt);
 }
 }
